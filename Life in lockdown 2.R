d33eadcc1d30a8373e4fae44709620436741bd78
khalid ansari <k23115229@kcl.ac.uk>
2024-02-18 17:00:48 +0000

    Create a summary table with mean number of false beliefs, for each level of facebook knowing

--- Life in lockdown 2.R
+++ Life in lockdown 2.R
@@ -3,3 +3,11 @@ library(readr)
 
 lul2_d <- read.csv('../data/life-under-lockdown-wave2-extract.csv')
 
+lul2_d %>%
+  mutate(know_facebook=ordered(know_facebook, 
+          levels=c('Nothing at all',
+                  'Not very much',
+                  'A fair amount',
+                  'A great deal'))) %>% 
+  group_by(know_facebook) %>%
+  summarise(n=n(),Beliefs=mean(beliefs))
